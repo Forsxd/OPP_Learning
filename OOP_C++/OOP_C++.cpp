e47ea2db9ef6ceb6c943cfedc1395819e59ed2bc
Forsxd <ioplkjhu1@yandex.ru>
2024-03-07 21:54:19 +0300

    Update OOP_C++.cpp

--- OOP_C++/OOP_C++.cpp
+++ OOP_C++/OOP_C++.cpp
@@ -13,15 +13,19 @@ private:
     string engineType;
     int cilinders;
 public:
-    Car(string carName, int hp, int tq, double lit, string engineT, int cil)
+    Car(string name, int bhp, int torque, double liters, string engineType, int cilinders)
     {
-        name = carName;
-        bhp = hp;
-        torque = tq;
-        liters = lit;
-        engineType = engineT;
-        cilinders = cil;
+        this->name = name;
+        this->bhp = bhp;
+        this->torque = torque;
+        this->liters = liters;
+        this->engineType = engineType;
+        this->cilinders = cilinders;
         cout << "Конструтор сконструировал " << name << endl;
+    }
+
+    void setBhp(int bhp) {
+        this->bhp = bhp;
     }
 
     Car()
@@ -82,15 +86,57 @@ public:
     }
 };
 
+class Test {
+private:
+    int Size;
+public:
+    int *data;
+    Test(int size) {
+        this->Size = size;
+        this->data = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = i;
+        }
+        cout << "data присвоена" << endl;
+    }
+    Test(const Test& other) {
+        cout << "copy of data присвоена" << endl;
+        this->Size = other.Size;
+        this->data = new int[other.Size];
+        for (int i = 0; i < other.Size; i++)
+        {
+            this->data[i] = other.data[i];
+        }
+    }
+    
+    ~Test() {
+        cout << "data отсвоена" << endl;
+        delete[] data;
+    }
+};
+
+void foo(Test value) {
+    cout << "функция вызвалась" << endl;
+}
+
+Test foo2() {
+    cout << "функция 2 вызвалась" << endl;
+    Test temp(2);
+    return temp;
+}
+
 int main()
 {
     setlocale(LC_ALL, "ru");
     srand(time(NULL));
-    Car BMW_M5("BMW M5 F90 Cometition", 625, 750, 4.4, "V", 8);
+    /*Car BMW_M5("BMW M5 F90 Cometition", 625, 750, 4.4, "V", 8);
     BMW_M5.carInfo();
-    Car Porshe911TS("Porshe 911 (992) Turbo S", 640, 800, 3.7, "F", 6);
-    Porshe911TS.carInfo();
-    Car placeHolder;
-    placeHolder.carInfo();
+    BMW_M5.setBhp(1500);
+    BMW_M5.carInfo();*/
+    /*Test dataTest(5);
+    foo(dataTest);*/
+    Test a(10);
+    Test b(a);
     return 0;
 }
